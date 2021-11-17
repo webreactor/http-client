a373094dd4534850bbf4297d3f69ce60e0d0e36a
Arsen Arutyunyan <a.arutyunyan@tanais.ru>
2021-11-17 11:31:36 +0300

    set cookies from response and send cookies

--- src/Middleware/Cookies.php
+++ src/Middleware/Cookies.php
@@ -6,13 +6,23 @@ class Cookies extends BaseMiddleware
 {
     public function action($request)
     {
+        $request[CURLOPT_COOKIE] = http_build_query($_COOKIE, '', ';');
+
         $response = parent::action($request);
 
         $extractedCookies = $this->extractResponseHeaderCookies(
             $response['response_header']
         );
 
-        // @todo
+        foreach ($extractedCookies as $cookie) {
+            setcookie(
+                $cookie['name'],
+                $cookie['value'],
+                strtotime($cookie['expires']),
+                $cookie['path'],
+                $cookie['domain'],
+            );
+        }
 
         return $response;
     }
@@ -31,15 +41,43 @@ class Cookies extends BaseMiddleware
                 )
             );
 
-            $cookieLineKeyValues = [];
-            foreach ($pieces as $part) {
+            $cookieAttributesValue = $this->getDefaultCookieAttributesValue();
+            foreach ($pieces as $part)
+            {
                 [$key, $value] = explode('=', $part, 2);
-                $cookieLineKeyValues[strtolower(trim($key))] = trim($value);
+
+                $key = trim($key);
+                $value = isset($value) ? trim($value) : true;
+
+                if (null === $cookieAttributesValue['name']) {
+                    $cookieAttributesValue['name'] = $key;
+                    $cookieAttributesValue['value'] = $value;
+                }
+                else {
+                    $key = strtolower($key);
+                    if (array_key_exists($key, $cookieAttributesValue)) {
+                        $cookieAttributesValue[$key] = $value;
+                    }
+                }
+
             }
 
-            $extractedCookies[] = $cookieLineKeyValues;
+            $extractedCookies[] = $cookieAttributesValue;
         }
 
         return $extractedCookies;
+    }
+
+    protected function getDefaultCookieAttributesValue(): array {
+        return [
+            'name' => null,
+            'value' => '',
+            'domain' => '',
+            'path' => '',
+            'max-age' => null,
+            'expires' => 0,
+            'secure' => false,
+            'httponly' => false
+        ];
     }
 }
